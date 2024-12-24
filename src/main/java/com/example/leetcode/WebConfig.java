298d00185d64e1577adc952d7b652c6416f413f2
Sparshgarg14 <ssparsh2000@gmail.com>
2024-12-24 19:57:18 +0530

    cors file changed

--- src/main/java/com/example/leetcode/WebConfig.java
+++ src/main/java/com/example/leetcode/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://portfolio-full-stack-five.vercel.app/")
+                        .allowedOrigins("https://portfolio-full-stack-five.vercel.app/","http://localhost:3000/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
